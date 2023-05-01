ba7eeb79d4bc310a4398459a67f284b15740b15f
Stas Kirpichnikov <kirpichnikovstas@gmail.com>
2023-05-01 18:41:40 +0300

    add range

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 const innerResult = document.querySelector('.inner__result');
 const btn = document.querySelector('.inner__btn');
+const minRange = document.querySelector('.range__min');
+const minText = document.querySelector('.range__min-result')
+const maxRange = document.querySelector('.range__max');
+const maxText = document.querySelector('.range__max-result')
+
+minText.textContent = minRange.value;
+maxText.textContent = maxRange.value;
+
+minRange.addEventListener('change', function () {
+    minText.textContent = this.value;
+})
+
+maxRange.addEventListener('change', function () {
+    maxText.textContent = this.value;
+})
 
 
-function randomNumber() {
-    const result = Math.floor(Math.random() * 10);
+function randomNumber(min, max) {
+    const result = Math.floor((Math.random() * (max - min) + min));
     innerResult.textContent = `Держи число ${result}`;
 }
 
-btn.addEventListener('click', randomNumber);
+btn.addEventListener('click', randomNumber(-10, 10));
+
